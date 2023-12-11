b134e9e5992edfd8dd21b5c1681a95334d945fb0
Yjsmall <1186907891@qq.com>
2023-12-11 11:16:52 +0800

    change a theme

---
+++ lua/plugins/oxocarbon.lua
@@ -0,0 +1,7 @@
+local colorscheme = require("lazyvim.plugins.colorscheme")
+return {
+  "nyoom-engineering/oxocarbon.nvim",
+  { "LazyVim/LazyVim", opts = {
+    colorscheme = "oxocarbon",
+  } },
+}
